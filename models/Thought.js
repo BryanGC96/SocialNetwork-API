105f759a9559d66922490edea1ae19ce258a32ad
BryanGC96 <2014104@icum.edu.mx>
2024-05-11 23:36:01 -0600

    Added 'Thought' model, and 'Reaction' (Schema only)

---
+++ models/Thought.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    }
+);
+
+// Getter method to formar timestamp.
+reactionSchema
+    .set('toJSON', { getters: true, virtuals: true });
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    }
+);
+
+thoughtSchema
+    .virtual('reactionSchema')
+    .get(function() {
+        return `${ this.reactions.length }`;
+    })
+    .set('toJSON', { getters: true, virtuals: true }); // Getter method to formar timestamp.
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
