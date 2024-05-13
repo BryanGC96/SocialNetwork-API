d4bb9749e70b58534f2a9a9ff0bc332698a2faa8
BryanGC96 <2014104@icum.edu.mx>
2024-05-12 22:46:10 -0600

    Added more logic to friend controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -50,6 +50,39 @@ module.exports = {
 
     // Update a thought by its _id.
     async updateThoughtById(req, res) {
+        try {
+            const updatedThought = await Thought.findByIdAndUpdate(
+                req.params.thoughtId,
+                req.body,
+                { new: true }
+            );
+            if (!updatedThought) {
+                return res.status(404).json({ message: 'No thought found with that ID' });
+            }
+            res.json(updatedThought);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
 
+    // Delete thought by its _id.
+    async deleteThoughtById (req, res) {
+        try {
+            const deleteThought = await Thought.findByIdAndDelete(req.params.thoughtId);
+            if (!deleteThought) {
+                return res.status(404).json({ message: 'No thought find with the requested Id' });
+            }
+
+            // Removes thought's _id from the associated user thought array.
+            const user = await User.findById(deleteThought.userId);
+            if (user) {
+                user.thoughts.pull(deleteThought._id);
+                await user.save();
+            }
+
+            res.json({ message: 'Thought deleted successfully' });
+        } catch (err) {
+            res.status(400).json(err);
+        }
     }
-}
+};
